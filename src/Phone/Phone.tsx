1a0cfd2b88c83a634b6b23f4dd4722df5a6d317d
Jack Nugent <jacknugent@MacBookPro.hsd1.md.comcast.net>
2025-05-21 17:38:42 -0400

    make width smaller

--- src/Phone/Phone.tsx
+++ src/Phone/Phone.tsx
@@ -20,7 +20,7 @@ function Phone() {
         borderRadius: ".5rem",
         padding: ".25rem",
         aspectRatio: "9 / 16",
-        width: "clamp(300px, 30vw, 400px)",
+        width: "clamp(200px, 30vw, 400px)",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
